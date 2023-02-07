2f25e936e2d1c10fcc2e0bd3c1d02a418f1bd9af
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2023-02-07 18:13:59 +0700

    solved maxProfit with pinned solution

--- BestTimeToBuyAndSellStock/maxProfit.py
+++ BestTimeToBuyAndSellStock/maxProfit.py
@@ -1,15 +1,34 @@
-
+#pinned solution
 def maxProfit(prices):
-    res = 0
-    
-    for i in range(len(prices)):
-        for j in range(len(prices)):
-            if  j > i and prices[i] < prices[j] :
-                if prices[j] - prices[i] > res:
-                    res = prices[j] - prices[i]
-               
-                
-    return res
+    # for some reason leetcode give me array of length =1 
+    if len(prices) < 2:
+        return 0
+    leftPtr = 0
+    rightPtr = 1
+    profit = prices[rightPtr] - prices[leftPtr]
+    i = 0 
+    while i < len(prices)-1:
+        #currentProfit is the profit of the current pair (current iteration)
+        left = prices[leftPtr]
+        right = prices[rightPtr]
+        
+        currentProfit = right - left
+        print(left,right,currentProfit)
+        if left > right:
+            leftPtr = rightPtr 
+            rightPtr += 1
+        else:
+            rightPtr += 1
+        if currentProfit > profit:
+            profit = currentProfit
+        
+            
+        
+        i+= 1
+    #in case it is non profitable
+    if profit < 0:
+        return 0 
+    return profit
     
     
     
@@ -17,3 +36,4 @@ def maxProfit(prices):
     
     
 print(maxProfit([3,2,6,5,0,3]))
+# print(maxProfit([7,1,5,3,6,4]))
