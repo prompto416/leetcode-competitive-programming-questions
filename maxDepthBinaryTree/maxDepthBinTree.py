478f4e9f161a1d345e325adab3b946966b577ccc
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2023-02-12 02:17:58 +0700

    solved maxDepthBinaryTree with recursive method

---
+++ maxDepthBinaryTree/maxDepthBinTree.py
@@ -0,0 +1,32 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def maxDepth( root):
+    if not root:
+        return 0
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+
+    return 1 + max((maxDepth(root.left)),maxDepth(root.right))
+    
+    
+
+        
+            
+    
+    
+    
+    
+
+
+a = TreeNode(3,TreeNode(9),TreeNode(20,TreeNode(15),TreeNode(7)))
+
+print(maxDepth(a))
+
+
