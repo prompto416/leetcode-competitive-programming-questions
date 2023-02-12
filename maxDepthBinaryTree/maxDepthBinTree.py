9b050961254f4631cd2638cfb0ae267c48cb2578
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2023-02-12 14:30:12 +0700

    commented submission code for maxDepth

--- maxDepthBinaryTree/maxDepthBinTree.py
+++ maxDepthBinaryTree/maxDepthBinTree.py
@@ -16,17 +16,17 @@ def maxDepth( root):
     return 1 + max((maxDepth(root.left)),maxDepth(root.right))
     
     
-
-        
-            
-    
-    
-    
-    
-
-
 a = TreeNode(3,TreeNode(9),TreeNode(20,TreeNode(15),TreeNode(7)))
 
 print(maxDepth(a))
 
+#to submit you need this 
+# def maxDepth(self, root):
+#         if not root:
+#             return 0
+#         """
+#         :type root: TreeNode
+#         :rtype: int
+#         """
 
+#         return 1 + max((self.maxDepth(root.left)),self.maxDepth(root.right))
