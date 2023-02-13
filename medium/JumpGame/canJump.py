282d3debb69d5c4a25ebc2180e8bceef258fc46c
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2023-02-13 18:28:26 +0700

    solved Jump Game with nick white dp method

--- medium/JumpGame/canJump.py
+++ medium/JumpGame/canJump.py
@@ -1,36 +1,12 @@
-def canJump( nums):
-    if len(nums) == 1:
-        return True
-    temp = nums.copy()
-    z = [] 
-    flag = None
-    i = 0 
-    while 0 in temp:
-        cur_z = temp.index(0)
-        z.append(cur_z)
-        temp[cur_z] = -1
-    if len(z) < 1:
-        return True
+def canJump(nums):
     
-    # while z != []:
-    
-    while z!= []:
-        for i in reversed(range(z[0])):
-            distance = z[0] - i
-            val = nums[i]
-            try:
-                
-                if (len(z) == 1 and (nums.index(z[0]) == len(nums)-1 )):
-                    print(True)
-            except:
-                print(z[0])
-            if val > distance:
-                flag = True
-                break
-            else:
-                flag = False
-        z.pop()
-
-    return flag
-
-print(canJump([2,0]))
+    lastgood = len(nums)-1
+    print(nums,lastgood)
+    for i in reversed(range(len(nums)-1)):
+        if i + nums[i] >= lastgood:
+            print(i,nums[i],lastgood)
+            lastgood = i
+        print(False,i,nums[i],lastgood)
+    return lastgood == 0
+nums = [3,2,1,0,4]
+canJump(nums)
