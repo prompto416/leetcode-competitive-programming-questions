bd2e1e92e0dfd42ac496eddd8515a58545ff7ed0
Phobphoomin Siriboon <90683904+prompto416@users.noreply.github.com>
2023-02-14 02:36:09 +0700

    solved jumpgame again with different code same apparoch

--- medium/JumpGame/canJump.py
+++ medium/JumpGame/canJump.py
@@ -10,3 +10,24 @@ def canJump(nums):
     return lastgood == 0
 nums = [3,2,1,0,4]
 canJump(nums)
+
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) == 1:
+            return True 
+        
+        flag = False
+        lastGood = len(nums)-1
+        for i in reversed(range(len(nums)-1)):
+            if i + nums[i] >= lastGood:
+                lastGood = i 
+                flag = True 
+            else:
+                flag = False
+        return flag
+      
